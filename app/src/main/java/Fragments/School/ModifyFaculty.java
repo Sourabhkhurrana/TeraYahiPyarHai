095cf0f4b5ffa28673772cc52ced5d115571bc12
sourabh khurana <sourabhkhurrana@gmail.com>
2017-04-09 02:10:47 +0530

    update 3

--- app/src/main/java/Fragments/School/ModifyFaculty.java
+++ app/src/main/java/Fragments/School/ModifyFaculty.java
@@ -1,11 +1,12 @@
 package Fragments.School;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 import com.nougatstudio.terayahipyarhai.R;
 
@@ -14,6 +15,9 @@ import com.nougatstudio.terayahipyarhai.R;
  */
 public class ModifyFaculty extends Fragment {
 
+    View root;
+    CardView cardView1, cardView2;
+    ScrollView scrollView1;
 
     public ModifyFaculty() {
         // Required empty public constructor
@@ -24,7 +28,25 @@ public class ModifyFaculty extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_modify_faculty, container, false);
+
+        root = inflater.inflate(R.layout.fragment_modify_faculty, container, false);
+        cardView1 = (CardView) root.findViewById(R.id.cardView1);
+        cardView2 = (CardView) root.findViewById(R.id.cardView2);
+
+        //DisableProgressBar();
+        EnableProgressBar();
+
+        return root;
+    }
+
+    private void EnableProgressBar() {
+        cardView1.setAlpha(0.5f);
+        cardView2.setAlpha(0.5f);
+    }
+
+    private void DisableProgressBar() {
+        cardView1.setAlpha(0);
+        cardView2.setAlpha(0);
     }
 
 }
