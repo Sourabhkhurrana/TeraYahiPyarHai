4d15982b38a78c656bfb23b3d501f2edb86f5a96
NirajSolanki <niraj.solanki.121@gmail.com>
2017-04-09 02:14:06 +0530

    Update102

--- app/src/main/java/com/nougatstudio/terayahipyarhai/SchoolProfile.java
+++ app/src/main/java/com/nougatstudio/terayahipyarhai/SchoolProfile.java
@@ -8,13 +8,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class SchoolProfile extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_school_profile);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
