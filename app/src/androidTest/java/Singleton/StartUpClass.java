0afd4ab41d5797ceb42a2a22b57e32561f8bec84
sourabh khurana <sourabh khurana>
2017-04-08 11:38:05 +0530

    Initial commit (base 08/04/2017)

---
+++ app/src/androidTest/java/Singleton/StartUpClass.java
@@ -0,0 +1,23 @@
+package Singleton;
+
+import android.app.Application;
+
+//import com.firebase.client.Firebase;
+import com.google.firebase.crash.FirebaseCrash;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by Neeraj Solanki on 7/16/2016.
+ */
+
+public class StartUpClass extends Application {
+    //@Override
+  /*  public void onCreate() {
+        super.onCreate();
+        if (Firebase.getDefaultConfig().isPersistenceEnabled() == false) {
+            Firebase.getDefaultConfig().setPersistenceEnabled(true);
+            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        }
+        //Fabric.with(this, new Crashlytics());
+    }*/
+}
